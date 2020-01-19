e317895022740007beac6abb7aee3674b5b3e274
Kairui Song <kasong@redhat.com>
2020-01-19 22:49:41 +0800

    Only print zone info parsing failure on debug mode

--- src/proc.c
+++ src/proc.c
@@ -105,7 +105,7 @@ int parse_keyword(int until, FILE *file, char *buf, const char* keyword, const c
 	}
 
 	if (!read) {
-		log_error("Failed to read %s\n", keyword);
+		log_debug("Failed to read %s\n", keyword);
 		return -EAGAIN;
 	}
 
